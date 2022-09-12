94420cb9983edaeaf78280e0b8f3eb3854568774
zoe jenkins <86444265+zoejenkins@users.noreply.github.com>
2022-09-12 00:42:01 +0000

    beginning to code

--- main.py
+++ main.py
@@ -1,16 +1,81 @@
 print("Welcome to the bookstore baby")
-books = {'Harry Potter': '12'}
+books = {
+  'Harry Potter': {
+    'Pages: ' : 600,
+    'Author: ': 'JK Rowling',
+    'Published : ' : 2000,
+    'Genre: ' : 'Fantasy'
+  }
+}
+
 
 def main():
-  choice = input("Please enter: \n (A) To Add a book \n (M)   To Modify the number of pages of a book \n (L) to Print out the number of pages of a book \n (P) to Print out all      books’ titles with their authors and the number of pages \n (Q) to Quit");
-  if choice == "a":
-    add();
+  quit = False;
+  while quit == False:
+    choice = input("Please enter: \n (A) To add a book \n (M)   To modify the number of pages of a book \n (L) To print out the number of pages of a book \n (P) To print out all books’ titles with their authors and the number of pages \n (Q) to Quit \n");
+    
+    if (choice == "a") or (choice == "A"):
+      add();
+      stoploop();
+      
+    elif (choice == "m") or (choice == "M"):
+      modify();
+      stoploop();
+      
+    elif (choice == "l") or (choice == "L"):
+      printpage();
+      stoploop();
+      
+    elif (choice == "p") or (choice == "P"):
+      printall()
+      stoploop();
+      
+    elif (choice == "q") or (choice == "Q"):
+      quit = True;
+      print("Goodbye!");
+
+    else:
+      print("What went wrong");
+
 
 def add():
-  add_book = input("Add a book: ")
-  add_page = input("How many pages? ")
-  books[add_book] = add_page
+  introductions = ['Title: ', 'Author: ', 'Pages: ', 'Genre: ', 'Year: ']
+  title = input("Enter the books' name: ")
+  author = input("Enter the author of {}: ".format(title))
+  year = input("Enter the year {} was published: ".format(title))
+  genre = input("Enter the genre of {}: ".format(title))
+  pages = input("Enter how many pages {} has: ".format(title))
+  intro_values = [str(title), str(author), str(pages), str(genre), str(year)]
+  new_book = dict(zip(introductions,intro_values))
+  books.update(new_book)
   print(books)
 
-def addingfunction(adding):
-  
+def remove():
+  book_remove = input("Which book would you like to remove?: ")
+  books.pop()
+
+def modify():
+  thisdict["year"] = input("How many pages does the book have?")
+
+def printpage():
+  print("printpage")
+
+def printall():
+  print(books)
+
+
+def stoploop():
+  end = input("Would you liek to continue? Y/N: ")
+  ok = False;
+  while ok == False:
+    if end == "N":
+      quit = True;
+      ok = True;
+    elif end == "Y":
+      quit = False;
+      ok = True;
+    else:
+      end = input("Invalid entry. please enter \"Y\" or \"N\"")
+
+
+main()
