b13ec97d41b60d808888baaf9f440cead8da165e
zoe jenkins <86444265+zoejenkins@users.noreply.github.com>
2022-09-16 08:18:03 +0000

    comments

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# Welcomes user to library
 print("Welcome to the library!")
 
+# Creates a dictionary for all of the books in the library
 books = {
+  # Nested dictionary of example book
   'Harry Potter': {
     'Author: ' : 'J.K. Rowling',
     'Pages: ' : '600',
@@ -9,11 +12,10 @@ books = {
   }
 }
 
-
 def add():
   """
-  Function for adding a book
-  Takes in values from users inputs and adds them to the 'books' dictionary
+  Function for adding a book to the library
+  Takes in values from users inputs and adds them to the 'books' dictionary as a nested dictionary
   """
   introductions = ['Author: ', 'Pages: ', 'Genre: ', 'Year: ']
   title = input("Enter the books' name: ")
@@ -28,6 +30,9 @@ def add():
   print(books)
 
 def remove():
+  """
+  function that removes a book (nested dictionary) of the user's choice from the book library ('books' dictionary)
+  """
   book_remove = str(input("Which book would you like to remove?: "))
   if book_remove in books:
     books.pop(book_remove)
@@ -35,6 +40,9 @@ def remove():
     print("That book isn't in the library")
 
 def modify():
+  """
+  function that lets user access and modify the number of pages in a book of their choice
+  """
   booktomodify = str(input("For which book do you want to modify the number of pages? "))
   if booktomodify in books:
     print(booktomodify + " currently has " + books[booktomodify]["Pages: "] + " pages.")
@@ -43,9 +51,12 @@ def modify():
     print(booktomodify + " now has " + books[booktomodify]["Pages: "] + " pages.")
 
   elif booktomodify not in books:
-    print("That book does not exist in the library")
+    print("That book does not exist in the library. Make sure you spelled everything correctly.")
 
 def printpage():
+  """
+  function that prints out how many pages a specific book has from the 'books' dictionary, chosen by the user
+  """
   pagetoprint = str(input("For which book do you want to know the number of pages? "))
   if pagetoprint in books:
     print('{} has '.format(pagetoprint) + books[pagetoprint]['Pages: '] + ' pages.')
@@ -53,9 +64,15 @@ def printpage():
     print("That book does not exist in the library")
 
 def printall():
+  """
+  function that prints out the entire 'books' dictionary
+  """
   print(books)
 
 def main():
+  """
+  main function of the program that runs on a loop; asks users to select which function they would like to use, uses if/else to determine which function to run, runs the chosen function from there
+  """
   quit = True
   while quit == True:
     choice = input("Please enter: \n (A) To add a book to the library \n (M) To modify the number of pages in an existing book \n (L) To print out the number of pages of a book \n (R) To remove a book from the library \n (P) To print out a list of every book in the library \n (Q) To quit \n");
@@ -83,4 +100,6 @@ def main():
       print("Invalid option, try again.")
 
 
+
+# calls the main function
 main()
